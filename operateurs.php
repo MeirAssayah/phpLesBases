a473e3c8a0b549eb1b2a44694104176847bc0a81
MeirAssayah <Meir.ASSAYAH@ortmontreuil.fr>
2025-01-28 18:08:15 +0100

    voici les bases

---
+++ operateurs.php
@@ -0,0 +1,25 @@
+<?php
+
+//Faire de l'incrementation
+
+$item=0;
+
+//1
+$item+=1;
+echo $item;
+
+//2
+$item=$item+1;
+echo $item;
+
+//3
+$item++;
+echo $item;
+
+echo"Incrementation de la variable \$item=".$item;
+
+//Operation de calcul
+$nbPersonne=10;
+$total=((10+1)-$nbPersonne)*2;
+//2
+echo"Total des personnes : ".$total;
